37a048c17b92f382bb469d99afb55eae85390835
Sayyaddsameer <147791861+Sayyaddsameer@users.noreply.github.com>
2023-10-19 10:48:14 +0530

    #include<stdio.h> int main() {     int n,k,m;     scanf("%d%d%d",&n,&k,&m);     if (n%2==0 && n%(m*k)==0)     {         printf("%d",n/(m*k));     }     else     {         printf("%d",(n/(m*k))+1);     } }

---
+++ Bags_and_Candies.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,k,m;
+    scanf("%d%d%d",&n,&k,&m);
+    if (n%2==0 && n%(m*k)==0)
+    {
+        printf("%d",n/(m*k));
+    }
+    else
+    {
+        printf("%d",(n/(m*k))+1);
+    }
+}
