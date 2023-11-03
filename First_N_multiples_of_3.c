b6bc2f294991e4b7b2c8d85d465d4f7e585ae5a7
Sayyaddsameer <147791861+Sayyaddsameer@users.noreply.github.com>
2023-11-03 11:37:59 +0530

    #include<stdio.h> int main() {     int i,n;     scanf("%d",&n);     for(i=3;i<=(3*n);i++)     {         if(i%3==0)         {             printf("%d ",i);         }     } }

---
+++ First_N_multiples_of_3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    scanf("%d",&n);
+    for(i=3;i<=(3*n);i++)
+    {
+        if(i%3==0)
+        {
+            printf("%d ",i);
+        }
+    }
+}
