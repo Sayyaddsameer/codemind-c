e097fd3c2383294b652f9799402907d1f80955b7
Sayyaddsameer <147791861+Sayyaddsameer@users.noreply.github.com>
2023-11-04 19:12:38 +0530

    #include <stdio.h>
    int main() {
    
      int n, reverse = 0, remainder;
      scanf("%d", &n);
       int a=n;
      while (n != 0)
      {
        remainder = n % 10;
        reverse = reverse * 10 + remainder;
        n /= 10;
      }
      if(a==reverse)
      {
          printf("Palindrome");
      }
      else
      {
          printf("Not Palindrome");
      }
    }

---
+++ Palindrome.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+
+  int n, reverse = 0, remainder;
+  scanf("%d", &n);
+   int a=n;
+  while (n != 0)
+  {
+    remainder = n % 10;
+    reverse = reverse * 10 + remainder;
+    n /= 10;
+  }
+  if(a==reverse)
+  {
+      printf("Palindrome");
+  }
+  else
+  {
+      printf("Not Palindrome");
+  }
+}
