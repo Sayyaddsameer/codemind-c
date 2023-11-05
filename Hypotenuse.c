e6a539b7cdce2feed74dfbd48b631f170fff1065
Sayyaddsameer <147791861+Sayyaddsameer@users.noreply.github.com>
2023-11-05 17:59:08 +0530

    #include<stdio.h> #include<math.h> int main() {     double a,b,c;     scanf("%lf%lf",&a,&b);     c=sqrt(pow(a,2)+pow(b,2));     printf("%.2lf",c); }

--- Hypotenuse.c
+++ Hypotenuse.c
@@ -2,8 +2,8 @@
 #include<math.h>
 int main()
 {
-    int a,b;
-    scanf("%d%d",&a,&b);
-    float c=sqrt(pow(a,2)+pow(b,2));
-    printf("%.2f",c);
+    double a,b,c;
+    scanf("%lf%lf",&a,&b);
+    c=sqrt(pow(a,2)+pow(b,2));
+    printf("%.2lf",c);
 }
