e60427dbb65297cfcae30bec00a4a53aff705083
Sayyaddsameer <147791861+Sayyaddsameer@users.noreply.github.com>
2023-11-23 10:53:01 +0530

    #include<stdio.h> int main() {     int n,r,s=0;     scanf("%d",&n);     while(n!=0)     {         r=n%10;         s+=r;         n/=10;     }     printf("%d",s); }

--- Sum_of_all_digits.c
+++ Sum_of_all_digits.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-	int i,n,r,d=0;
-	scanf("%d",&n);
-	while(n!=0)
-	{
-		r=n%10;
-		d=d+r;
-		n=n/10;
-	}
-	printf("%d",d);
+    int n,r,s=0;
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        r=n%10;
+        s+=r;
+        n/=10;
+    }
+    printf("%d",s);
 }
