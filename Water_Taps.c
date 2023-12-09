bc65ded8fa8c6d0a6c5f71d66a1e2ec9b3003e32
sayyadsameer <147791861+Sayyaddsameer@users.noreply.github.com>
2023-12-09 11:25:05 +0530

    #include<stdio.h> int time(int,int); int main() {     int x,y;     scanf("%d%d",&x,&y);     int res=time(x,y);     printf("%d",res); } int time(int x,int y) {     int t=(x*y)/(x+y);     return t; }

--- Water_Taps.c
+++ Water_Taps.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
+int time(int,int);
 int main()
 {
     int x,y;
     scanf("%d%d",&x,&y);
-    printf("%d",(x*y)/(x+y));
+    int res=time(x,y);
+    printf("%d",res);
+}
+int time(int x,int y)
+{
+    int t=(x*y)/(x+y);
+    return t;
 }
