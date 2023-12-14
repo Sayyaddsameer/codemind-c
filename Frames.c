0aed71e9f5f7991d04cd8516c3019aa65c584c69
sayyadsameer <147791861+Sayyaddsameer@users.noreply.github.com>
2023-12-14 08:25:22 +0530

    #include<stdio.h> int cost(int n,int m, int x) {     int c=2*(n+m)*x;     return c; } int main() {     int n,m,x;     scanf("%d%d%d",&n,&m,&x);     int res =cost(n,m,x);     printf("%d",res); }

--- Frames.c
+++ Frames.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
+int cost(int n,int m, int x)
+{
+    int c=2*(n+m)*x;
+    return c;
+}
 int main()
 {
     int n,m,x;
-    scanf("%d%d%d",&m,&n,&x);
-    int cost=2*(n+m)*x;
-    printf("%d",cost);
+    scanf("%d%d%d",&n,&m,&x);
+    int res =cost(n,m,x);
+    printf("%d",res);
 }
