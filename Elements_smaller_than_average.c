b97ade231add0e2e746ae1fdcefcddd1a2e2cb9d
sayyadsameer <147791861+Sayyaddsameer@users.noreply.github.com>
2024-01-03 17:29:08 +0530

    #include<stdio.h> int main() {     int i,n,s=0,avg,c=0;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         s=s+arr[i];     }     avg=s/n;     for(i=0;i<n;i++)     {         if(arr[i]<=avg)         {             c++;         }     }     printf("%d",c); }

---
+++ Elements_smaller_than_average.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,s=0,avg,c=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        s=s+arr[i];
+    }
+    avg=s/n;
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]<=avg)
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
