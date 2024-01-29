6d3d89a47f835e3e549664fca3845577a05352f6
sayyadsameer <147791861+Sayyaddsameer@users.noreply.github.com>
2024-01-29 18:10:50 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         for(j=1;j<=i;j++)         {             printf("%d",i);         }         for(j=n;j>=1;j--)         {             if(i<j)printf("*",i);         }         printf(" ");     } }

---
+++ Basic_Pattern_5.3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    for(i=n;i>=1;i--)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%d",i);
+        }
+        for(j=n;j>=1;j--)
+        {
+            if(i<j)printf("*",i);
+        }
+        printf("
+");
+    }
+}
